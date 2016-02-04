fb58b244f0f55b341010784e8672e9a53357afc1
noahmcmlln <noahmcmlln@gmail.com>
2016-02-04 16:41:11 -0500

    Import - Scott's house function
    changed Sketch.js in one blank p5 project to Scott’s house function

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
-var setup = function setup() {
+function setup () {
+  createCanvas(600, 600);
 
-}
+  var houseX = 100;
+  var houseY = 300;
+  var houseWidth = 200;
+  var houseHeight = 200;
+  var roofHeight = 100;
 
-var draw = function () {
+  var drawHouse = function () {
+   rect(houseX, houseY, houseWidth, houseHeight); // Draw a structure
+   triangle(houseX - 10, houseY, houseWidth / 2 + houseX, houseY - roofHeight, houseX + houseWidth + 10, houseY); // Draw a roof
+  };
 
+  drawHouse();
+
+  houseX = houseX + 250; // Increase x by 250
+
+  drawHouse();
 }
