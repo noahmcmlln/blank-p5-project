2c9b2ead9a76df5befda2c62e93a7207ce3d3b51
Noah McMullen <noahmcmlln@gmail.com>
2016-04-18 19:30:34 -0400

    Revert "Import - Scott's house function"
    This reverts commit fb58b244f0f55b341010784e8672e9a53357afc1.

--- sketch.js
+++ sketch.js
@@ -1,20 +1,7 @@
-function setup () {
-  createCanvas(600, 600);
+var setup = function setup() {
 
-  var houseX = 100;
-  var houseY = 300;
-  var houseWidth = 200;
-  var houseHeight = 200;
-  var roofHeight = 100;
+}
 
-  var drawHouse = function () {
-   rect(houseX, houseY, houseWidth, houseHeight); // Draw a structure
-   triangle(houseX - 10, houseY, houseWidth / 2 + houseX, houseY - roofHeight, houseX + houseWidth + 10, houseY); // Draw a roof
-  };
+var draw = function () {
 
-  drawHouse();
-
-  houseX = houseX + 250; // Increase x by 250
-
-  drawHouse();
 }
