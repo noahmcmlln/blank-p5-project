680da27109749a95e855fcca0472c6afb3c65c59
Noah McMullen <noahmcmlln@gmail.com>
2016-04-27 16:12:17 -0400

    Refactored House

--- sketch.js
+++ sketch.js
@@ -1,22 +1,41 @@
-function setup () {
+function setup() {
   createCanvas(600, 600);
-  background("blue");
+  background("blue"); // sky
 
   fill("green");
   rect(0, 400, 600, 200);
 
-  fill(255);
-  rect(200, 200, 200, 200);
-
-  fill(0);
-  rect(320, 100, 40, 100);
-
-  fill(150);
-  triangle(180, 200, 300, 100, 420, 200);
-
-  fill("red");
-  rect(275, 300, 50, 100);
-
   fill("yellow");
   ellipse(500, 100, 100, 100);
+
+  makeahouse();
+}
+
+function makeahouse() {
+  var houseColor = 255;
+  var chimColor = 0;
+  var roofColor = 150;
+  var houseX = 200;
+  var houseY = 200;
+  var houseSize = 200;
+  var chimX = 320;
+  var chimWidth = 40;
+  var roofOverhang = 20;
+  var houseMaxY = 100;
+  var doorHalfWidth = 25;
+  var doorHeight = 100;
+  var middle = 300;
+
+
+  fill(houseColor); // house body
+  rect(houseX, houseY, houseSize, houseSize);
+
+  fill(chimColor); // chimney
+  rect(chimX, houseMaxY, chimWidth, houseMaxY);
+
+  fill(roofColor); // roof
+  triangle(houseX - roofOverhang, houseY, middle, houseMaxY, houseX * 2 + roofOverhang, houseY);
+
+  fill("red"); // door
+  rect(middle - doorHalfWidth, middle, doorHalfWidth * 2, doorHeight);
 }
